0d27af6da92c22e6474cb301b517922fe9e45385
Kaikai Liu <lkk688@gmail.com>
2023-07-09 08:10:56 -0700

    update huggingfacetest

--- pytorch/huggingfacetest.py
+++ pytorch/huggingfacetest.py
@@ -3,6 +3,7 @@ from tqdm.auto import tqdm
 import numpy as np
 import evaluate
 import os
+import pandas as pd
 
 from transformers import pipeline
 from transformers import AutoTokenizer, DataCollatorWithPadding, DataCollatorForTokenClassification
@@ -54,6 +55,41 @@ if __name__ == "__main__":
 
     specific_model = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
     print(specific_model(data))
+
+    text = """Dear Amazon, last week I ordered an Optimus Prime action figure \
+    from your online store in Germany. Unfortunately, when I opened the package, \
+    I discovered to my horror that I had been sent an action figure of Megatron \
+    instead! As a lifelong enemy of the Decepticons, I hope you can understand my \
+    dilemma. To resolve the issue, I demand an exchange of Megatron for the \
+    Optimus Prime figure I ordered. Enclosed are copies of my records concerning \
+    this purchase. I expect to hear from you soon. Sincerely, Bumblebee."""
+    classifier = pipeline("text-classification")
+    outputs = classifier(text)
+    print(pd.DataFrame(outputs))
+
+    ner_tagger = pipeline("ner", aggregation_strategy="simple")
+    outputs = ner_tagger(text)
+    print(pd.DataFrame(outputs))
+
+    reader = pipeline("question-answering")
+    question = "What does the customer want?"
+    outputs = reader(question=question, context=text)
+    print(pd.DataFrame([outputs]))
+
+    summarizer = pipeline("summarization")
+    outputs = summarizer(text, max_length=45, clean_up_tokenization_spaces=True)
+    print(outputs[0]['summary_text'])
+
+    translator = pipeline("translation_en_to_de", 
+                      model="Helsinki-NLP/opus-mt-en-de")
+    outputs = translator(text, clean_up_tokenization_spaces=True, min_length=100)
+    print(outputs[0]['translation_text'])
+
+    generator = pipeline("text-generation")
+    response = "Dear Bumblebee, I am sorry to hear that your order was mixed up."
+    prompt = text + "\n\nCustomer service response:\n" + response
+    outputs = generator(prompt, max_length=200)
+    print(outputs[0]['generated_text'])
 
     #pip install xformers
     #pip3 install emoji==0.6.0
