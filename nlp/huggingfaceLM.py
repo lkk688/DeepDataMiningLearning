eab63e7e8da8c5aaf38aa59d85eee5eb88046601
Kaikai Liu <kaikai.liu@sjsu.edu>
2023-11-13 09:43:05 -0800

    new train of LM

--- nlp/huggingfaceLM.py
+++ nlp/huggingfaceLM.py
@@ -16,26 +16,35 @@ import math
 import collections
 import numpy as np
 
+global data_field
+global block_size
+block_size = 128
 
 class TokenizerWrapper:
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
     
     def tokenize_function(self, examples):
-        return self.tokenizer(
-            [" ".join(x) for x in examples["answers.text"]],
+        result =  self.tokenizer(
+            [" ".join(x) for x in examples[data_field]], #"answers.text"
             padding="max_length",
             truncation=True,
         )
+        if self.tokenizer.is_fast: #always fast
+            result["word_ids"] = [result.word_ids(i) for i in range(len(result["input_ids"]))] #one to one map to word_ids
+            #each token is mapped to the word it originates from, if some tokens belongs to the same word, it will show the duplicate the wordids
+        return result
 
 def tokenize_function(examples):
-    result = tokenizer(examples["text"]) #generate "input_ids" list and "attention_mask"
+    result = tokenizer(examples[data_field]) #"text" generate "input_ids" list and "attention_mask"
     if tokenizer.is_fast:
         result["word_ids"] = [result.word_ids(i) for i in range(len(result["input_ids"]))] #one to one map to word_ids
+        #each token is mapped to the word it originates from, if some tokens belongs to the same word, it will show the duplicate the wordids
     return result
 
+#split the concatenated sequences into shorter chunks defined by block_size, which should be both shorter than the maximum input length and short enough for your GPU RAM.
 def group_texts(examples):
-    block_size = 128
+    #block_size = 128
     # Concatenate all texts.
     #print(examples.keys())
     concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
@@ -93,30 +102,33 @@ def insert_random_mask(batch):
 def testpredictmask(model, text):
     #text = "This is a great [MASK]."
     inputs = tokenizer(text, return_tensors="pt")
-    token_logits = model(**inputs).logits #torch.Size([1, 8, 30522])
+    print(inputs)
+    token_logits = model(**inputs).logits #torch.Size([1, 8, 30522]) shape (batch_size, sequence_length, hidden_size)
     # Find the location of [MASK] and extract its logits
-    mask_token_index = torch.where(inputs["input_ids"] == tokenizer.mask_token_id)[1]
+    findmask=torch.where(inputs["input_ids"] == tokenizer.mask_token_id)
+    mask_token_index = findmask[1] #[1]
     mask_token_logits = token_logits[0, mask_token_index, :] #torch.Size([1, 30522])
     # Pick the [MASK] candidates with the highest logits
     top_5_tokens = torch.topk(mask_token_logits, 5, dim=1).indices[0].tolist()
     return top_5_tokens
 
+#data_name="imdb", dataconfig="", model_checkpoint="distilbert-base-uncased"
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='simple distributed training job')
     parser.add_argument('--data_type', type=str, default="huggingface",
                     help='data type name: huggingface, custom')
-    parser.add_argument('--data_name', type=str, default="imdb",
+    parser.add_argument('--data_name', type=str, default="eli5",
                     help='data name: eli5, imdb')
-    parser.add_argument('--dataconfig', type=str, default="",
+    parser.add_argument('--dataconfig', type=str, default="asks",
                     help='train_asks[:5000]')
     parser.add_argument('--subset', type=float, default=0,
                     help='0 means all dataset')
     parser.add_argument('--data_path', type=str, default=r"E:\Dataset\NLPdataset\aclImdb",
                     help='path to get data')
-    parser.add_argument('--model_checkpoint', type=str, default="distilbert-base-uncased",
+    parser.add_argument('--model_checkpoint', type=str, default="distilroberta-base",
                     help='Model checkpoint name from https://huggingface.co/models, "bert-base-cased", "distilbert-base-uncased" "cardiffnlp/twitter-roberta-base-emotion"')
-    parser.add_argument('--task', type=str, default="token_classifier",
+    parser.add_argument('--task', type=str, default="LM",
                     help='NLP tasks: sentiment, token_classifier, "sequence_classifier", custom_classifier')
     parser.add_argument('--outputdir', type=str, default="./output",
                     help='output path')
@@ -157,13 +169,24 @@ if __name__ == "__main__":
         taskname=args.traintag #taskname="eli5asksciencemodeling"
 
     if args.data_type == "huggingface":
-        datasetpath="/data/cmpe249-fa23/Huggingfacecache/imdb/plain_text/1.0.0/d613c88cf8fa3bab83b4ded3713f1f74830d1100e171db75bbddb80b3345c9c0"
         if USE_HPC:
-            #raw_datasets = load_dataset(args.data_name, cache_dir=mycache_dir) #eli5
-            trainarrowpath=os.path.join(mycache_dir, datasetpath, args.data_name+'-train.arrow')
-            #valarrowpath=os.path.join(mycache_dir, datasetpath, args.data_name+'-validation.arrow')
-            testarrowpath=os.path.join(mycache_dir, datasetpath, args.data_name+'-test.arrow')
-            raw_datasets = load_dataset("arrow", data_files={'train': trainarrowpath, 'test': testarrowpath})
+            if args.data_name=='imdb':
+                datasetpath="/data/cmpe249-fa23/Huggingfacecache/imdb/plain_text/1.0.0/d613c88cf8fa3bab83b4ded3713f1f74830d1100e171db75bbddb80b3345c9c0"
+                #raw_datasets = load_dataset(args.data_name, cache_dir=mycache_dir) #eli5
+                trainarrowpath=os.path.join(mycache_dir, datasetpath, args.data_name+'-train.arrow')
+                #valarrowpath=os.path.join(mycache_dir, datasetpath, args.data_name+'-validation.arrow')
+                testarrowpath=os.path.join(mycache_dir, datasetpath, args.data_name+'-test.arrow')
+                raw_datasets = load_dataset("arrow", data_files={'train': trainarrowpath, 'test': testarrowpath})
+                data_field='text'
+                sampletext = "This is a great [MASK]."
+            elif args.data_name=='eli5':
+                datasetpath="/data/cmpe249-fa23/Huggingfacecache/eli5/LFQA_reddit/1.0.0/17574e5502a10f41bbd17beba83e22475b499fa62caa1384a3d093fc856fe6fa"
+                trainarrowpath=os.path.join(mycache_dir, datasetpath, args.data_name+'-train'+'_'+args.dataconfig+'.arrow')#eli5-train_asks.arrow
+                testarrowpath=os.path.join(mycache_dir, datasetpath, args.data_name+'-test'+'_'+args.dataconfig+'.arrow')
+                raw_datasets = load_dataset("arrow", data_files={'train': trainarrowpath, 'test': testarrowpath})
+                data_field='answers.text'
+                #sampletext = "A static force applied eccentric to the center of [MASK]."#mass
+                sampletext = "The Milky Way is a <mask> galaxy."
         else:
             if not args.dataconfig:
                 raw_datasets = load_dataset(args.data_name) #dataconfig="train_asks[:5000]"
@@ -177,7 +200,7 @@ if __name__ == "__main__":
         raw_datasets = raw_datasets.flatten() #nested structure become flat: answers.text
         #features: ['q_id', 'title', 'selftext', 'document', 'subreddit', 'answers.a_id', 'answers.text', 'answers.score', 'title_urls.url', 'selftext_urls.url', 'answers_urls.url']
 
-        print("All keys in raw datasets:", raw_datasets.keys())
+        print("All keys in raw datasets:", raw_datasets['train'][0].keys())
     
     global tokenizer
     if USE_HPC:
@@ -186,6 +209,7 @@ if __name__ == "__main__":
     else:
         tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)#, cache_dir=mycache_dir)
     tokenizer.model_max_len=512
+    tokenizer_wrapper = TokenizerWrapper(tokenizer)
 
     if USE_HPC:
         localpath=os.path.join(mycache_dir, model_checkpoint) #modelname="distilroberta-base"
@@ -197,11 +221,14 @@ if __name__ == "__main__":
     print(f"'>>> Model number of parameters: {round(model_num_parameters)}M'")
     #print(f"'>>> BERT number of parameters: 110M'")
 
-    text = "This is a great [MASK]."
-    top_5_tokens = testpredictmask(model, text)
+    examples=raw_datasets["train"][0]
+    listexamples = [" ".join(x) for x in examples[data_field]]
+    
+    top_5_tokens = testpredictmask(model, sampletext)
+    token_train=tokenizer(listexamples)
 
     for token in top_5_tokens:
-        print(f"'>>> {text.replace(tokenizer.mask_token, tokenizer.decode([token]))}'")
+        print(f"'>>> {sampletext.replace(tokenizer.mask_token, tokenizer.decode([token]))}'")
 
     # tokenizer(raw_datasets["train"][0]['answers.text'])
     # tokenizer_wrapper = TokenizerWrapper(tokenizer)
@@ -211,9 +238,10 @@ if __name__ == "__main__":
     # lm_dataset = tokenized_dataset.map(group_texts, batched=True, num_proc=4)
 
     # Use batched=True to activate fast multithreading!
-    tokenized_datasets = raw_datasets.map(
-        tokenize_function, batched=True, remove_columns=["text", "label"]
-    )
+    # tokenized_datasets = raw_datasets.map(
+    #     tokenize_function, batched=True, remove_columns=["text", "label"]
+    # )
+    tokenized_datasets = raw_datasets.map(tokenizer_wrapper.tokenize_function, batched=True, num_proc=3, remove_columns=raw_datasets["train"].column_names)
     # Slicing produces a list of lists for each feature
     tokenized_samples = tokenized_datasets["train"][:3] #generate "input_ids" list and "attention_mask", add "word_ids"
 
@@ -230,7 +258,7 @@ if __name__ == "__main__":
     lm_datasets = tokenized_datasets.map(group_texts, batched=True)
 
     #tokenizer.pad_token = tokenizer.eos_token #tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-    print(tokenizer.eos_token) #None
+    print(tokenizer.eos_token) #None </s>
     print(tokenizer.pad_token) #[PAD]
 
     #mlm_probability means the percentage of [MASK]
@@ -253,13 +281,14 @@ if __name__ == "__main__":
     downsampled_dataset = lm_datasets["train"].train_test_split(test_size=0.2)
 
     batch_size = args.batch_size #64
-    downsampled_dataset = downsampled_dataset.remove_columns(["word_ids"])
+    #downsampled_dataset = downsampled_dataset.remove_columns(["word_ids"])
+    test_dataset = downsampled_dataset["test"].remove_columns(["word_ids"])
     
     if InsertMask:
-        eval_dataset = downsampled_dataset["test"].map(
+        eval_dataset = test_dataset.map(
             insert_random_mask,
             batched=True,
-            remove_columns=downsampled_dataset["test"].column_names,
+            remove_columns=test_dataset.column_names,
         )
         eval_dataset = eval_dataset.rename_columns(
             {
@@ -269,7 +298,7 @@ if __name__ == "__main__":
             }
         )
     else:
-        eval_dataset=downsampled_dataset["test"]
+        eval_dataset=test_dataset #downsampled_dataset["test"]
     
     train_dataloader = DataLoader(
         downsampled_dataset["train"],
