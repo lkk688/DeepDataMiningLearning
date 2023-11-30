845a038e89b184e1b88a85caf3fb5aa0f545faea
Kaikai Liu <kaikai.liu@sjsu.edu>
2023-11-29 21:30:14 -0800

    add signalAI

---
+++ dataapps/gradiotest.py
@@ -0,0 +1,9 @@
+import gradio as gr
+
+def greet(name):
+    return "Hello " + name + "!"
+
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+    
+if __name__ == "__main__":
+    demo.launch(show_api=False)   
