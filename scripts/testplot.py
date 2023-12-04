e01ab45a8392cc3ea791965dd0be98a967208bc8
Kaikai Liu <lkk688@gmail.com>
2023-12-04 08:35:13 -0800

    add torchaudio

--- scripts/testplot.py
+++ scripts/testplot.py
@@ -22,12 +22,34 @@ ax.legend(title='Fruit color')
 plt.show()
 
 
+
+#import matplotlib.pyplot as plt 
+import numpy as np 
+from matplotlib.colors import LogNorm 
+      
+dx, dy = 0.015, 0.05
+y, x = np.mgrid[slice(-4, 4 + dy, dy), 
+                slice(-4, 4 + dx, dx)] 
+z = (1 - x / 3. + x ** 5 + y ** 5) * np.exp(-x ** 2 - y ** 2) 
+z = z[:-1, :-1] 
+z_min, z_max = -np.abs(z).max(), np.abs(z).max() 
+  
+c = plt.imshow(z, cmap ='Greens', vmin = z_min, vmax = z_max, 
+                 extent =[x.min(), x.max(), y.min(), y.max()], 
+                    interpolation ='nearest', origin ='lower') 
+plt.colorbar(c) 
+  
+plt.title('matplotlib.pyplot.imshow() function Example',  
+                                     fontweight ="bold") 
+plt.show() 
+
+
 # Python code to read image
 import cv2
  
 # To read image from disk, we use
 # cv2.imread function, in below method,
-img = cv2.imread("../sampledata/sjsuimag1.jpg", cv2.IMREAD_COLOR)
+img = cv2.imread("sampledata/sjsuimag1.jpg", cv2.IMREAD_COLOR)
  
 # Creating GUI window to display an image on screen
 # first Parameter is windows title (should be in string format)
