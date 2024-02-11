cc2dba3d585d052aaa2dc3ca6570d7b2da84e0f2
Kaikai Liu <kaikai.liu@sjsu.edu>
2024-02-10 18:58:14 -0800

    update torch models

--- DeepDataMiningLearning/torchdata.py
+++ DeepDataMiningLearning/torchdata.py
@@ -4,9 +4,38 @@ import torch
 import torchvision
 import torchvision.datasets as datasets
 import os
-mytorchvisiondata='/data/cmpe249-fa23/torchvisiondata/'
+mytorchvisiondata='data/'#'/data/cmpe249-fa23/torchvisiondata/'
 
 print(torch.__version__)
+
+from torchvision.transforms import v2
+
+H, W = 32, 32
+img = torch.randint(0, 256, size=(3, H, W), dtype=torch.uint8)
+
+transforms = v2.Compose([
+    v2.RandomResizedCrop(size=(224, 224), antialias=True),
+    v2.RandomHorizontalFlip(p=0.5),
+    v2.ToDtype(torch.float32),
+    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+img = transforms(img)
+print(img.shape)
+print(type(img)) #<class 'torch.Tensor'>
+
+# Detection (re-using imports and transforms from above)
+from torchvision import tv_tensors
+img = torch.randint(0, 256, size=(3, H, W), dtype=torch.uint8)
+boxes = torch.randint(0, H // 2, size=(3, 4))
+boxes[:, 2:] += boxes[:, :2]
+boxes = tv_tensors.BoundingBoxes(boxes, format="XYXY", canvas_size=(H, W))
+
+# The same transforms can be used!
+img, boxes = transforms(img, boxes)
+# And you can pass arbitrary input structures
+output_dict = transforms({"image": img, "boxes": boxes})
+
+
 cifar_trainset = datasets.CIFAR10(root=mytorchvisiondata, train=True, download=True, transform=None)
 print(cifar_trainset)
 #Extracting /data/cmpe249-fa22/torchvisiondata/cifar-10-python.tar.gz to /data/cmpe249-fa22/torchvisiondata/
--- dataapps/openaiapi_imaggen.py
+++ dataapps/openaiapi_imaggen.py
@@ -2,10 +2,11 @@
 
 from openai import OpenAI
 client = OpenAI()
-
+#"a computer system diagram for professional research papers"
+#"1024x1024"
 response = client.images.generate(
   model="dall-e-3",
-  prompt="a computer system diagram for professional research papers",
+  prompt="Create an image that captures the spirit of Chinese New Year. The background should be a vibrant Chinese red, leaving some open spaces. A majestic Chinese dragon should be prominently featured, dancing and swirling amidst the scenery with joy and vitality. The words 'Happy Chinese New Year!', should be accurate and placed strategically within the image. The overall atmosphere should be filled with festive cheer, welcoming the new year with hope, peace, and prosperity.",
   size="1024x1024",
   quality="standard",
   n=1,
