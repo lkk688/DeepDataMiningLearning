932a29ff307a676fb1bcac1dea5e528a6edba92c
Kaikai Liu <kaikai.liu@sjsu.edu>
2024-03-30 16:13:54 -0700

    update install_postnvidiacontainer.sh

--- docker/install_postnvidiacontainer.sh
+++ docker/install_postnvidiacontainer.sh
@@ -14,13 +14,15 @@ else
     sudo groupadd docker
     echo "The 'docker' group has been created."
 fi
+echo "Docker group finished"
 addusercommand="sudo usermod -aG docker $USER"
 #sudo usermod -aG docker $USER
 # Execute the commands
 eval "$addusercommand" || true  # Execute command1 and ignore errors
-
+echo "Added docker user"
 #log out and log back again, or run the following command
-newgrp docker
+#newgrpcommand="newgrp docker"
+#eval "$newgrpcommand" || true
 echo "Finished docker rootless"
 echo "Start installing NVIDIA Container toolkit"
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
@@ -30,4 +32,11 @@ curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dear
 
 sudo apt-get update
 sudo apt-get install -y nvidia-container-toolkit
+#configure the container runtime via nvidia-ctk
+echo "Finished nvidia-container toolkit installation"
+#nvidia-ctk runtime configure --runtime=docker --config=$HOME/.config/docker/daemon.json
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
+sudo nvidia-ctk config --set nvidia-container-cli.no-cgroups --in-place
 echo "Finished"
+
