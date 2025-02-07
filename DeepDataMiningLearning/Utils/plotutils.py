0b1a286354d8040137b129704136c8e8a4fb88fc
Kaikai Liu <kaikai.liu@sjsu.edu>
2025-02-07 13:32:33 -0800

    fix colab link error

--- DeepDataMiningLearning/Utils/plotutils.py
+++ DeepDataMiningLearning/Utils/plotutils.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import os
+
 figsize = (12, 8)
 fontsize = 15
 outputfolder = 'output/'
@@ -35,10 +37,48 @@ def plotcomparisonbar(data1, data2, labels, tick_labels, xlabel='Questions', yla
     plt.savefig(os.path.join(outputfolder, figname), format='pdf')  # Save the figure as a pdf file
     plt.show()
 
-"""answering comparision between FastAPI and old. The question length were small, medium and large
-as the question size increases the timetaken and also increased and the fastapi was faster than the old implementation
-"""
-if __name__ == "__main__":
+
+def plotVectors(vecs, cols, alpha=1):
+    """
+    Plot set of vectors.
+
+    Parameters
+    ----------
+    vecs : array-like
+        Coordinates of the vectors to plot. Each vector is in an array. For
+        instance: [[1, 3], [2, 2]] can be used to plot 2 vectors.
+    cols : array-like
+        Colors of the vectors. For instance: ['red', 'blue'] will display the
+        first vector in red and the second in blue.
+    alpha : float
+        Opacity of vectors
+
+    Returns:
+
+    fig : instance of matplotlib.figure.Figure
+        The figure of the vectors
+    """
+    plt.axvline(x=0, color='#A9A9A9', zorder=0)
+    plt.axhline(y=0, color='#A9A9A9', zorder=0)
+
+    for i in range(len(vecs)):
+        if (isinstance(alpha, list)):
+            alpha_i = alpha[i]
+        else:
+            alpha_i = alpha
+        if (len(vecs[i])==2):
+            x = np.concatenate([[0,0],vecs[i]])
+        elif (len(vecs[i])==4):
+            x = vecs[i]
+        plt.quiver([x[0]],
+                   [x[1]],
+                   [x[2]],
+                   [x[3]],
+                   angles='xy', scale_units='xy', scale=1, color=cols[i],
+                  alpha=alpha_i)
+
+
+def testcompare():
     #from https://colab.research.google.com/drive/1h-a6QwyMaIz7cJnsoaagQM1KTSU8fNCa#scrollTo=nqKJ6pkPuw3f
     plotcomparisonbar(data1=[12.775,  15.78,  49.5], data2=[13.367, 15.76, 51.77], \
                     labels=['FastAPI in Pods','Direct Pytorch Inference'],\
@@ -73,3 +113,49 @@ if __name__ == "__main__":
                   labels=['FastAPI in Pods','Direct Pytorch Inference'],\
                   tick_labels=["Case1", "Case2", "Case2"], xlabel='Sentence Length', ylabel='Time Taken (s)', \
                   figname='Translationfromspanishtoenglishcomparision.pdf')
+
+if __name__ == "__main__":
+    errorModel1 = [22, -4]
+    errorModel2 = [14, 9]
+    plotVectors([errorModel1, errorModel2], [sns.color_palette()[0], sns.color_palette()[1]])
+
+    plt.xlim(-1, 25)
+    plt.ylim(-5, 10)
+    plt.show()
+    
+    u = np.array([0,0,1,6])
+    v = np.array([0,0,4,2])
+    w = u+v
+
+    u_bis = [u[2], u[3], v[2], v[3]]
+
+    plotVectors([u, u_bis, w],
+                [sns.color_palette()[0],
+                sns.color_palette()[1],
+                sns.color_palette()[2]])
+
+    plt.xlim(-2, 6)
+    plt.ylim(-2, 9)
+
+    plt.text(-1, 3.5, r'$||\vec{u}||$', color=sns.color_palette()[0], size=20)
+    plt.text(2.5, 7.5, r'$||\vec{v}||$', color=sns.color_palette()[1], size=20)
+    plt.text(2, 2, r'$||\vec{u}+\vec{v}||$', color=sns.color_palette()[2], size=20)
+
+    plt.show()
+    plt.close()
+    
+    x = [0,0,0,2]
+    y = [0,0,2,2]
+
+    plotVectors([x, y], [sns.color_palette()[0], sns.color_palette()[1]])
+
+    plt.xlim(-1, 3)
+    plt.ylim(-1, 3)
+
+    plt.text(-0.5, 1, r'$\vec{x}$', size=18, color=sns.color_palette()[0])
+    plt.text(1.5, 0.5, r'$\vec{y}$', size=18, color=sns.color_palette()[1])
+    
+    plt.show()
+    plt.close()
+
+
