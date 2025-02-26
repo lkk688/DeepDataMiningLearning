838066247dd8c199c77e9e70b9049ee91f835c37
Kaikai Liu <kaikai.liu@sjsu.edu>
2025-02-26 10:42:35 -0800

    add cuda install script

---
+++ docs/source/annotation.md
@@ -0,0 +1,98 @@
+
+# Label Studio
+Label Studio is an open-source data labeling tool. It's designed to help you label various types of data (images, text, audio, etc.) for machine learning model training.
+
+https://labelstud.io/guide/install
+
+Poetry helps you declare, manage, and install the libraries your Python project depends on. It ensures you have the correct versions of those libraries. It replaces older methods that could be cumbersome, like managing requirements.txt files. Poetry also assists in packaging your Python project for distribution. This means it helps you create packages that others can easily install and use.
+
+Uses pyproject.toml for project configuration. The poetry install command is crucial for setting up your project's environment.
+* Generates a poetry.lock file to ensure consistent installations across different environments. 
+* The poetry.lock file plays a crucial role here. It stores the exact versions of all installed packages, ensuring consistent installations across different environments.
+
+
+A fundamental pyproject.toml file will typically include these sections:
+* [tool.poetry]: This section contains Poetry-specific settings, such as your project's name, version, and dependencies.
+* [build-system]: This section defines the build system requirements.
+* [project]: This section contains core metadata about your project.
+
+`https://github.com/HumanSignal/label-studio/blob/develop/label_studio/__init__.py`: This file signifies that the label_studio directory is a Python package. 
+* In Python, the `__init__.py` file serves two primary purposes: 1) Package Marker: Its presence in a directory tells Python that the directory should be treated as a Python package; 2) It can be used to execute initialization code when the package is imported. It can also be used to control what is imported when someone uses `from xx import *`.
+* `importlib.metadata` is a module in the Python standard library (since Python 3.8) that provides tools for accessing metadata about installed Python packages. It allows you to retrieve information like the package's version, dependencies, entry points
+
+
+```bash
+git clone https://github.com/HumanSignal/label-studio.git
+
+# install dependencies
+cd label-studio
+pip install poetry
+poetry install
+
+# run db migrations
+# poetry run tells Poetry to execute a command within the project's virtual environment.
+poetry run python label_studio/manage.py migrate
+
+# collect static files
+poetry run python label_studio/manage.py collectstatic
+
+# start the server in development mode at http://localhost:8080
+poetry run python label_studio/manage.py runserver
+
+```
+
+
+```bash
+npx create-react-app my-labeling-app
+cd my-labeling-app
+npm install react react-dom
+git clone https://github.com/HumanSignal/label-studio.git
+cp -r label-studio/web/libs/editor src/label-studio-editor
+cd src/label-studio-editor
+npm install
+cd../../
+# create src/App.js
+npm start
+#When you're ready to deploy your application, you'll need to create a production build using npm run build.
+```
+
+Import and use the editor component:
+```bash
+// src/App.js
+import React from 'react';
+import LabelStudio from './label-studio-editor';
+
+function App() {
+  return (
+    <div>
+      <LabelStudio />
+    </div>
+  );
+}
+
+export default App;
+```
+
+# Setup in Intel13Rack
+```bash
+lkk@lkk-intel13rack:~$ curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o Miniconda3-latest-Linux-x86_64.sh
+lkk@lkk-intel13rack:~$ python3 -V
+Python 3.10.12
+lkk@lkk-intel13rack:~$ bash Miniconda3-latest-Linux-x86_64.sh -b -u
+lkk@lkk-intel13rack:~$ source ~/miniconda3/bin/activate
+(base) lkk@lkk-intel13rack:~$ conda init bash
+(base) lkk@lkk-intel13rack:~$ gcc --version
+gcc (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0
+```
+
+update GCC11 to GCC13
+```bash
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt update
+sudo apt install gcc-13 g++-13
+gcc --version
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 13
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 13
+gcc --version
+gcc (Ubuntu 13.1.0-8ubuntu1~22.04) 13.1.0
+```
---
+++ scripts/cuda_local_install.sh
@@ -0,0 +1,102 @@
+# Syntax cuda_install CUDA_VERSION INSTALL_PREFIX EXPORT_TO_BASH
+#   CUDA_VERSION in {110, 111, 112, 113, 114, 115, 116, 117, 118, 120, 121, 122, 123, 124, 125, 126, 128}
+#   EXPORT_TO_BASH in {0, 1} with 0=False and 1=True
+
+# For example, the following installs CUDA 12.6 to ~/nvidia/cuda-12.6 and exports the path to your .bashrc
+
+#bash install_cuda.sh 126 ~/nvidia 1
+
+URL110=https://developer.download.nvidia.com/compute/cuda/11.0.3/local_installers/cuda_11.0.3_450.51.06_linux.run
+URL111=https://developer.download.nvidia.com/compute/cuda/11.1.1/local_installers/cuda_11.1.1_455.32.00_linux.run
+URL112=https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda_11.2.2_460.32.03_linux.run
+URL113=https://developer.download.nvidia.com/compute/cuda/11.3.1/local_installers/cuda_11.3.1_465.19.01_linux.run
+URL114=https://developer.download.nvidia.com/compute/cuda/11.4.4/local_installers/cuda_11.4.4_470.82.01_linux.run
+URL115=https://developer.download.nvidia.com/compute/cuda/11.5.2/local_installers/cuda_11.5.2_495.29.05_linux.run
+URL116=https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/cuda_11.6.2_510.47.03_linux.run
+URL117=https://developer.download.nvidia.com/compute/cuda/11.7.1/local_installers/cuda_11.7.1_515.65.01_linux.run
+URL118=https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
+URL120=https://developer.download.nvidia.com/compute/cuda/12.0.1/local_installers/cuda_12.0.1_525.85.12_linux.run
+URL121=https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda_12.1.1_530.30.02_linux.run
+URL122=https://developer.download.nvidia.com/compute/cuda/12.2.2/local_installers/cuda_12.2.2_535.104.05_linux.run
+URL123=https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/cuda_12.3.2_545.23.08_linux.run
+URL124=https://developer.download.nvidia.com/compute/cuda/12.4.1/local_installers/cuda_12.4.1_550.54.15_linux.run
+URL125=https://developer.download.nvidia.com/compute/cuda/12.5.1/local_installers/cuda_12.5.1_555.42.06_linux.run
+URL126=https://developer.download.nvidia.com/compute/cuda/12.6.0/local_installers/cuda_12.6.0_560.28.03_linux.run 
+
+#updated from https://developer.nvidia.com/cuda-12-6-0-download-archive?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=runfile_local
+
+CUDA_VERSION=$1
+BASE_PATH=$2
+EXPORT_BASHRC=$3
+
+if [[ -n "$CUDA_VERSION" ]]; then
+  if   [[ "$CUDA_VERSION" -eq "110" ]]; then
+    URL=$URL110
+    FOLDER=cuda-11.0
+  elif [[ "$CUDA_VERSION" -eq "111" ]]; then
+    URL=$URL111
+    FOLDER=cuda-11.1
+  elif [[ "$CUDA_VERSION" -eq "112" ]]; then
+    URL=$URL112
+    FOLDER=cuda-11.2
+  elif [[ "$CUDA_VERSION" -eq "113" ]]; then
+    URL=$URL113
+    FOLDER=cuda-11.3
+  elif [[ "$CUDA_VERSION" -eq "114" ]]; then
+    URL=$URL114
+    FOLDER=cuda-11.4
+  elif [[ "$CUDA_VERSION" -eq "115" ]]; then
+    URL=$URL115
+    FOLDER=cuda-11.5
+  elif [[ "$CUDA_VERSION" -eq "116" ]]; then
+    URL=$URL116
+    FOLDER=cuda-11.6
+  elif [[ "$CUDA_VERSION" -eq "117" ]]; then
+    URL=$URL117
+    FOLDER=cuda-11.7
+  elif [[ "$CUDA_VERSION" -eq "118" ]]; then
+    URL=$URL118
+    FOLDER=cuda-11.8
+  elif [[ "$CUDA_VERSION" -eq "120" ]]; then
+    URL=$URL120
+    FOLDER=cuda-12.0
+  elif [[ "$CUDA_VERSION" -eq "121" ]]; then
+    URL=$URL121
+    FOLDER=cuda-12.1
+  elif [[ "$CUDA_VERSION" -eq "122" ]]; then
+    URL=$URL122
+    FOLDER=cuda-12.2
+  elif [[ "$CUDA_VERSION" -eq "123" ]]; then
+    URL=$URL123
+    FOLDER=cuda-12.3
+  elif [[ "$CUDA_VERSION" -eq "124" ]]; then
+    URL=$URL124
+    FOLDER=cuda-12.4
+  elif [[ "$CUDA_VERSION" -eq "125" ]]; then
+    URL=$URL125
+    FOLDER=cuda-12.5
+  elif [[ "$CUDA_VERSION" -eq "126" ]]; then
+    URL=$URL126
+    FOLDER=cuda-12.6
+  else
+    echo "argument error: No cuda version passed as input. Choose among versions 110 to 126"
+  fi
+else
+    echo "argument error: No cuda version passed as input. Choose among versions 110 to 126"
+fi
+
+FILE=$(basename $URL)
+
+if [[ -n "$CUDA_VERSION" ]]; then
+  echo $URL
+  echo $FILE
+  wget $URL
+  bash $FILE --no-drm --no-man-page --override --toolkitpath=$BASE_PATH/$FOLDER/ --toolkit --silent
+  if [ "$EXPORT_BASHRC" -eq "1" ]; then
+    echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$BASE_PATH/$FOLDER/lib64" >> ~/.bashrc
+    echo "export PATH=\$PATH:$BASE_PATH/$FOLDER/bin" >> ~/.bashrc
+    source ~/.bashrc
+  fi
+else
+  echo ""
+fi
